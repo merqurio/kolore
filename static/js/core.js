0cc45dafbdfb887a559853fecf232e12bf917c26
Gabi Merqurio <gabriel.maeztu@gmail.com>
2014-10-29 16:25:41 +0100

    Final Bower Integration

--- static/js/core.js
+++ static/js/core.js
@@ -436,9 +436,7 @@ $(document).ready(function () {
     //***********************************BEGIN Function calls *****************************	
 
     //***********************************BEGIN Function calls *****************************	
-    $('.animate-number').each(function () {
-        $(this).animateNumbers($(this).attr("data-value"), true, parseInt($(this).attr("data-animation-duration"), 10));
-    });
+    
     $('.animate-progress-bar').each(function () {
         $(this).css('width', $(this).attr("data-percentage"));
 
