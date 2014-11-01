23ca4ce83df17560a58fe98f86627890717d3e52
Gabi Merqurio <gabriel.maeztu@gmail.com>
2014-11-01 21:36:10 +0100

    Working ! But Model not :(

--- app/admin/static/js/add-wizard.js
+++ app/admin/static/js/add-wizard.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
     var upNumbers = $('ul.wizard-steps li'),
         wizPanels = $('.tab-pane'),
         allNextBtn = $('.next');
+        allPrevBtn = $('.previous');
 
     wizPanels.hide();
 
@@ -12,10 +13,10 @@ $(document).ready(function () {
     upNumbers.click(function (e) {
         e.preventDefault();
         // Gets tabs id based in the href of the anchor
-        var $target = $($(this).children('a').attr('href')),
+        var $target = $($(this).attr('data-target')),
             $item = $(this);
 
-        if (!$item.hasClass('disabled')) {
+        if (!$item.hasClass('disabled')){
             upNumbers.removeClass('active');
             $item.addClass('active');
             wizPanels.hide();
@@ -25,10 +26,11 @@ $(document).ready(function () {
 
     // Function called if NEXT button is clicked
     allNextBtn.click(function(){
+        // Gets all the data for next tab
         var curStep = $(this).closest(".tab-pane"),
             curStepBtn = curStep.attr("id"),
             nextStepWizard = $('ul.wizard-steps li a[href="#' + curStepBtn + '"]').parent().next().children("a");
-            curInputs = curStep.find("input[type='text'],input[type='url']"),
+            curInputs = curStep.find("input[type='text']"),
             isValid = true;
 
         $(".form-group").removeClass("has-error");
@@ -39,9 +41,22 @@ $(document).ready(function () {
             }
         }
 
-        if (isValid)
-            nextStepWizard.removeAttr('disabled').trigger('click');
+        if (isValid){
+            nextStepWizard.parent('li').removeClass('disabled');
+            nextStepWizard.trigger('click');
+        }
     });
 
+    // Function called if PREVIOUS button is clicked
+    allPrevBtn.click(function(e) {
+        e.preventDefault();
+    
+        var curStep = $(this).closest(".tab-pane"),
+            curStepBtn = curStep.attr("id"),
+            prevStepWizard = $('ul.wizard-steps li a[href="#' + curStepBtn + '"]').parent().prev().children("a");
+        prevStepWizard.trigger('click')
+    });
+
+    // Initiliace first tab
     $('ul.wizard-steps li.active').trigger('click');
 });
