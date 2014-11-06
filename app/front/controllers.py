82859318764647f619a2c136b53d114e0f317da6
Gabi Merqurio <gabriel.maeztu@gmail.com>
2014-11-06 13:21:19 +0100

    update dependencies

--- app/front/controllers.py
+++ app/front/controllers.py
@@ -1,6 +1,12 @@
 # Imports
 # ----------------------------------------------------------------
 from flask import Blueprint, render_template, make_response, request
+from google.appengine.ext import ndb
+
+# Models
+# ----------------------------------------------------------------
+
+from app.admin.models import BlogPost
 
 # Config
 # ----------------------------------------------------------------
@@ -15,7 +21,8 @@ front_app = Blueprint('front', __name__, template_folder='templates',
 
 @front_app.route('/')
 def home():
-    response = make_response(render_template('front-index.html'))
+    posts = BlogPost.query().fetch()
+    response = make_response(render_template('front-index.html', posts=posts))
     if not request.cookies.get('lang'):
         response.set_cookie('lang', value='en')
     return response
