edad418ea0c26dc179ed8795e7b4465f4d7c9ad5
Gabi Merqurio <gabriel.maeztu@gmail.com>
2015-01-03 17:29:58 +0100

    minor updates

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Import the Flask Framework
 # ----------------------------------------------------------------
-from flask import Flask, render_template, request, redirect, url_for
+from flask import (Flask, render_template, request, redirect,
+                   url_for, make_response)
 app = Flask(__name__)
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
@@ -16,8 +17,7 @@ app.config['BABEL_DEFAULT_LOCALE'] = 'en'
 LANGUAGES = {
     'en': 'English',
     'es': 'Espanol',
-    'eu': 'Euskera',
-    'fr': 'Francais'
+    'eu': 'Euskera'
 }
 
 
@@ -48,7 +48,11 @@ def get_locale():
 def admin_redirect():
     return redirect(url_for('admin.home'))
 
-@app.errorhandler(404)
+
+@app.errorhandler(400)  # Bad Request
+@app.errorhandler(401)  # Unauthorized
+@app.errorhandler(403)  # Forbidden
+@app.errorhandler(404)  # Not Found
 def page_not_found(e):
     """Return a custom 404 error."""
     return render_template('404.html')
@@ -58,3 +62,13 @@ def page_not_found(e):
 def page_not_founds(e):
     """Return a custom 500 error."""
     return render_template('500.html')
+
+
+@app.route('/sitemap.xml')
+def sitemap():
+    response = make_response(render_template(
+        'sitemap.xml',
+        host_url=request.host_url[:-1]
+    ))
+    response.headers['Content-Type'] = 'application/xml'
+    return response
